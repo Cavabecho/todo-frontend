5000d74ea92a250d52bb6999029a49343174f4f0
Maksim Pegov <maksim.peg@gmail.com>
2022-10-19 21:31:28 +0200

    bug fix

--- components/my-component.jsx
+++ components/my-component.jsx
@@ -1,17 +1,29 @@
 
 function MyComponent() {
-   let [flag, setFlag] = React.useState(false);
-   const showTitle = () => {
-      setFlag(!flag);
-   }
+  const [flag, setFlag] = React.useState(false)
+  const [todos, setTodos] = React.useState([])
+  const showTitle = () => {
+    setFlag(!flag)
+  }
+
+  const handleClick = () => {
+    getTodos().then((todos) => {
+      setTodos(todos)
+    })
+  }
+  
+
   return (
     <div>
       {flag ? <h1>hello</h1> : null}
-      <button onClick={getTodos}>get buttoon</button>
+      <button onClick={handleClick}>get buttoon</button>
+      {todos.map((todo) => (
+        <h1>{todo.text}</h1>
+      ))}
       {/* <button onClick={getData}>add buttoon</button>
       <button onClick={getData}>edit buttoon</button>
       <button onClick={getData}>delete buttoon</button> */}
 
     </div>
-  );
+  )
 }
