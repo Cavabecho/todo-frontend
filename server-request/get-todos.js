e0aeac48f9103c6690aa3b9d06d5991722f5524a
Cavabecho <115113171+Cavabecho@users.noreply.github.com>
2022-10-20 23:50:25 +0200

    Create the 'add, delete, edit, get' files, file 'edit' doesnt work

--- server-request/get-todos.js
+++ server-request/get-todos.js
@@ -1,10 +1,11 @@
 
 async function getTodos() {
 
-    const response = await fetch('http://104.248.166.249:3000/todo').then((data) => data.json())
+    const response = await fetch('http://104.248.166.249:3000/todo')
+    .then((data) => data.json())
 
     console.log(response)
+    
     return response
-
 }
 
