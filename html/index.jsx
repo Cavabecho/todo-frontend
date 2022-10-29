5fc912f85d0708e13e52d54eb95b6f2021ec2992
Cavabecho <115113171+Cavabecho@users.noreply.github.com>
2022-10-29 15:32:12 +0200

    test

--- html/index.jsx
+++ html/index.jsx
@@ -1,18 +1,27 @@
+// function MyApp() {
+//   const [log, setLog] = React.useState(!false);
+
+//   const login = () => {
+//     setLog(!log);
+//   };
+
+//   return (
+//     <div className="root">
+//       {log ? <Todos logChange={login} />: <LogIn logIn={login} />}
+//     </div>
+//   );
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<MyApp />, rootElement);
+
 function MyApp() {
-  const [log, setLog] = React.useState(!false);
-
-  const login = () => {
-    setLog(!log);
-  };
-
   return (
     <div className="root">
-      {log ? <Todos logChange={login} />: <LogIn logIn={login} />}
+           
     </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<MyApp />, rootElement);
-
-
